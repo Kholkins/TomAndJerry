14edbbdac72675cadd31e131b47b37b332fe6c48
Kholkin <kholkins@mail.ru>
2020-03-20 18:32:38 +0300

    Tom&Jerry

--- app/src/main/java/com/example/tomandjerry/MainActivity.java
+++ app/src/main/java/com/example/tomandjerry/MainActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewPropertyAnimator;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageViewTom;
+    ImageView imageViewJerry;
 
     boolean a = true;
 
@@ -14,18 +18,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageViewTom = (ImageView)findViewById(R.id.imageViewTom);
+        imageViewJerry = (ImageView)findViewById(R.id.imageViewJerry);
     }
 
     public void animateTomJerry(View view) {
 
-        ImageView imageViewTom = (ImageView)findViewById(R.id.imageViewTom);
-        ImageView imageViewJerry = (ImageView)findViewById(R.id.imageViewJerry);
         if (a == true){
-            imageViewTom.animate().alpha(0).setDuration(3000);
-            imageViewJerry.animate().alpha(1).setDuration(3000);
+            imageViewTom.animate().rotation(imageViewTom.getRotation()+3600).scaleX(0.0f).scaleY(0.0f).alpha(0).setDuration(3000);
+            imageViewJerry.animate().rotation(imageViewJerry.getRotation()+3600).scaleX(1.0f).scaleY(1.0f).alpha(1).setDuration(3000);
         }else {
-            imageViewJerry.animate().alpha(0).setDuration(3000);
-            imageViewTom.animate().alpha(1).setDuration(3000);
+            imageViewJerry.animate().rotation(imageViewJerry.getRotation()+3600).scaleX(0.0f).scaleY(0.0f).alpha(0).setDuration(3000);
+            imageViewTom.animate().rotation(imageViewTom.getRotation()+3600).scaleX(1.0f).scaleY(1.0f).alpha(1).setDuration(3000);
         }
         a = !a;
     }
